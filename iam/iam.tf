6b72b3b6aba667bbfc6ac6c6cb7a1b9eb81c3790
romagtgt <grytsykroman@gmail.com>
2021-08-05 19:18:37 +0000

    terraform iam

---
+++ iam/iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_group" "sysusers" {
+  name = "sysusers"
+  
+
+}
+
+
+resource "aws_iam_user" "bob" {
+  name = "bob"
+
+  tags = {
+    Team      = "DevOps"
+  }
+
+}
+resource "aws_iam_user_group_membership" "example1" {
+  user = aws_iam_user.bob.name
+
+  groups = [
+    aws_iam_group.sysusers.name,
+  ]
+}
